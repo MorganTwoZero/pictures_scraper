83a1ea1f9bd101cb3b33f0cab48a11eda1240d88
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-06-08 06:43:30 +0200

    Add like button

--- frontend/src/views/PostsView.vue
+++ frontend/src/views/PostsView.vue
@@ -1,62 +1,56 @@
 <template>
-    <div v-for="post in posts" :key="post.post_link">
-      <PostsComponent :post="post"/>
-    </div>
+  <div v-for="post in posts" :key="post.post_link">
+    <PostsComponent :post="post" />
+  </div>
 </template>
-<script>
 
-import PostsComponent from '@/components/Posts.vue'
-import axios from 'axios';
-export default {
-  name: 'PostsView',
-  components: {
-    PostsComponent,
-  },
-  data() {
-    return {
-      posts: [],
-      page: 1,
-    };
-  },
-  methods: {
-    getMessage() {
-      axios.get(this.$route.path + '?page=' + this.page + '&offset=5')
-        .then((res) => {
-          this.posts = res.data;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-    getNextPosts() {
-      window.onscroll = () => {
-        if ((window.innerHeight + Math.ceil(window.scrollY)) >= document.body.offsetHeight) { //ceil for mobile
-          this.page++;
-          axios.get(this.$route.path + '?page=' + this.page + '&offset=5').then(response => {
-            this.posts = this.posts.concat(response.data);
-            });
-          }
-        }
-    },
-    clearPosts() {
-      this.posts = [];
-    },
-  },
-  beforeMount() {
-    this.getMessage();
-  },
-  mounted() {
-    this.getNextPosts();
-  },
+<script setup>
+import { onBeforeMount, onMounted, ref } from 'vue'
+import router from '@/router'
+
+import PostsComponent from '@/components/PostsComponent.vue'
+
+import axios from 'axios'
+
+let posts = ref([]);
+let page = 1;
+
+function getMessage() {
+  axios.get(router.currentRoute.value.fullPath + '?page=' + page + '&offset=5')
+    .then((res) => {
+      posts.value = res.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
+
+function getNextPosts() {
+  window.onscroll = () => {
+    if ((window.innerHeight + Math.ceil(window.scrollY)) >= document.body.offsetHeight) { //ceil for mobile
+      page++;
+      axios.get(router.currentRoute.value.fullPath + '?page=' + page + '&offset=5').then(response => {
+        posts.value = posts.value.concat(response.data);
+      });
+    }
+  }
+}
+
+onBeforeMount(() => {
+  getMessage();
+})
+
+onMounted(() => {
+  getNextPosts();
+})
 </script>
 
 <style scoped>
 div {
-    border-radius: 2px;
-    padding: 10px;
-    border: 1px solid rgb(0, 0, 0);
-    width: max-content;
-    height: max-content;
+  border-radius: 2px;
+  padding: 10px;
+  border: 1px solid rgb(0, 0, 0);
+  width: max-content;
+  height: max-content;
 }
 </style>
