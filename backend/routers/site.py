d214abbec0cae1e9c337ab570b7039f23b58a84f
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-07-03 23:25:56 +0200

    Added logger in site route

--- backend/routers/site.py
+++ backend/routers/site.py
@@ -2,6 +2,7 @@ import ast
 from datetime import datetime
 from typing import Sequence
 import requests
+import logging
 
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
@@ -14,30 +15,38 @@ from utils.crud.users import get_all_users_with_twitter, get_user_with_twitter
 from utils.crud.posts import get_posts, my_feed_db_get
 from security import get_current_user
 
+
+#Logging
+logger = logging.getLogger(__name__)
+
+#Routers
 router = APIRouter(
     prefix='/api',
     tags=["site"],
 )
 
 async def update(db: Session):
-    print('Start update ' + str(datetime.now()))
+    logger.info('Update started')
 
-    users = get_all_users_with_twitter(db)
-    posts =  await request_homeline_many_users(users)
-    homeline_save_many_users(db, posts)
-    print('Homeline updated')
+    try:
+        users = get_all_users_with_twitter(db)
+        posts =  await request_homeline_many_users(users)
+        homeline_save_many_users(db, posts)
+        logger.info('Homeline updated')
 
-    posts = await request()
-    twitter_save(db, posts.twitter_honkai)
-    print('Twitter updated')
-    pixiv_save(db, posts.pixiv)
-    print('Pixiv updated')
-    mihoyo_bbs_save(db, posts.bbs_mihoyo)
-    print('Mihoyo updated')
-    for html in posts.lofter:
-        lofter_save(db, html)
-    print('Lofter updated')
-    print('End update ' + str(datetime.now()))
+        posts = await request()
+        twitter_save(db, posts.twitter_honkai)
+        logger.info('Twitter updated')
+        pixiv_save(db, posts.pixiv)
+        logger.info('Pixiv updated')
+        mihoyo_bbs_save(db, posts.bbs_mihoyo)
+        logger.info('Mihoyo updated')
+        for html in posts.lofter:
+            lofter_save(db, html)
+        logger.info('Lofter updated')
+        logger.info('End update ' + str(datetime.now()))
+    except:
+        logger.exception('error')
 
 @router.get("/update")
 async def start_update(db: Session = Depends(get_db)):
@@ -51,6 +60,7 @@ def honkai_posts(
     offset: int = 5
     ):
 
+    logger.debug('Honkai posts requested')
     posts = get_posts(db, page, offset)
     return posts
 
@@ -62,6 +72,7 @@ async def homeline_posts(
     offset: int = 5
     ):
 
+    logger.debug('Myfeed requested')
     user_in_db: UserInDB = get_current_user(request, db)
     user: UserWithTwitter = get_user_with_twitter(user_in_db.username, db)
     if user.twitter_header:
@@ -75,6 +86,7 @@ def like(
     db: Session = Depends(get_db)
     ):
 
+    logger.debug('Like requested, post link={}'.format(post_link))
     post_id: int = int(post_link[-20:-1])
     user_in_db: UserInDB = get_current_user(request, db)
     user = get_user_with_twitter(user_in_db.username, db)
