44bb105c827ff6450a7e104d74309804972c8546
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-07-04 08:07:35 +0200

    Made sure every embed test checks for a single thing

--- backend/tests/routers_embed_test.py
+++ backend/tests/routers_embed_test.py
@@ -3,6 +3,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from routers.embed import parse_post_id
+from settings import settings
 
 
 @pytest.mark.vcr
@@ -13,16 +14,18 @@ def test_embed(client: TestClient):
     assert response.headers['content-type'] == 'image'
 
 @pytest.mark.vcr
-@pytest.mark.parametrize('pic_id, expected_status_code, expected_json', [
-    ('990835560', 404, {'error': 'Post not found, probably wrong id'}),
-    ('9908355q', 422, {'error': 'Post id or pic number is not an integer'}),
-    ])
-@pytest.mark.vcr
-def test_embed_wrong_id(client: TestClient, pic_id: int, expected_status_code: int, expected_json: dict[str, str]):
+def test_embed_id_not_found(client: TestClient):
     '''Test if the embed returns image'''
-    response = client.get('/api/embed/{pic_id}'.format(pic_id=pic_id))
-    assert response.status_code == expected_status_code
-    assert response.json() == expected_json
+    response = client.get('/api/embed/990835560')
+    assert response.status_code == 404
+    assert response.json() == {'error': 'Post not found, probably wrong id'}
+
+@pytest.mark.vcr
+def test_embed_id_not_int(client: TestClient):
+    '''Test if the embed returns image'''
+    response = client.get('/api/embed/9908355q')
+    assert response.status_code == 422
+    assert response.json() == {'error': 'Post id or pic number is not an integer'}
 
 @pytest.mark.vcr
 def test_embed_discord(client: TestClient):
@@ -34,7 +37,7 @@ def test_embed_discord(client: TestClient):
     assert response.headers['content-type'] == 'text/html; charset=utf-8'
     assert response.text.startswith('\n        <html>')
     assert response.text.endswith('</html>\n    ')
-    assert 'https://honkai-pictures.ru/api/embed/99083556.json' in response.text
+    assert settings.SITE_URL+'/api/embed/99083556.json' in response.text
     
 @pytest.mark.vcr
 def test_embed_json(client: TestClient):
@@ -46,7 +49,7 @@ def test_embed_json(client: TestClient):
     assert response.headers['content-type'] == 'application/json'
     assert response.json() == {
         'type':'image/jpeg',
-        'url':'https://honkai-pictures.ru/api/embed/99083556.jpg',
+        'url': settings.SITE_URL+'/api/embed/99083556.jpg',
         'author_name':'Source',
         'author_url':'https://www.pixiv.net/en/artworks/99083556'
         }
@@ -55,7 +58,6 @@ def test_embed_json(client: TestClient):
     ('99487795_p1', 99487795, 1),
     ('99487795_p0', 99487795, 0),
     ('99487795', 99487795, 0),
-    pytest.param('string', 1, 1, marks=pytest.mark.xfail)
 ])
 def test_embed_parse_id(
     requested_id: str, 
@@ -66,10 +68,21 @@ def test_embed_parse_id(
     assert parsed_id == post_id
     assert parsed_pic_num == pic_num
 
+@pytest.mark.parametrize('requested_id', [
+    'string',
+    '99487795_p',
+    '99487795_pq',
+    'qwer_p1',
+    '_p1',
+])
+def test_embed_invalid_parse_id(requested_id: str):
+
+    with pytest.raises(ValueError):
+        parse_post_id(requested_id)
+
 @pytest.mark.parametrize('user_agent, status_code', [
     ('Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)', 200),
     ('Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0', 200),
-    ('', 307),
 ])
 def test_embed_discord_useragent(client: TestClient, user_agent: str, status_code: int):
     '''Test for discord`s client/crawler detection'''
@@ -80,3 +93,12 @@ def test_embed_discord_useragent(client: TestClient, user_agent: str, status_cod
     )
 
     assert request.status_code == status_code
+
+def test_embed_not_discord_useragent(client: TestClient):
+    request = client.get(
+        '/api/embed/99083556',
+        allow_redirects=False,
+        headers={'user-agent': ''},
+    )
+
+    assert request.status_code == 307
