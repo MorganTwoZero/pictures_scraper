0b43ab312347afeeb968ff90bbeac90d2d1508f4
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-07-07 18:07:18 +0200

    utils/request.py tests

--- backend/tests/utils_request_test.py
+++ backend/tests/utils_request_test.py
@@ -1,4 +1,5 @@
 import pytest
+import httpx
 
 from db.schemas import RequestResults, UserWithTwitter
 from settings import settings
@@ -14,9 +15,40 @@ def user_with_twitter():
     return user
 
 @pytest.mark.vcr
-async def test_request():
+async def test_request_honkai():
     assert type(await request.request_honkai()) == RequestResults
 
 @pytest.mark.vcr
 async def test_request_homeline(user_with_twitter):
     assert await request.request_homeline([user_with_twitter])
+
+def test_request_urls_list():
+    assert len(request.urls) == 3 + len(request.LOFTER_TAGS.split(' '))
+
+@pytest.mark.vcr
+async def test_request_get():
+    client = httpx.AsyncClient()
+    response = await request._get(client, 'https://jsonplaceholder.typicode.com/posts')
+    assert response.status_code == 200
+
+async def test_request_get_timeout():
+    client = httpx.AsyncClient()
+    response = await request._get(client, 'http://10.255.255.1')
+    assert response.status_code == 400
+    assert response.text == 'Request timeout'
+
+@pytest.mark.vcr
+async def test_request_pixiv_proxy():
+    url = 'https://www.pixiv.net/touch/ajax/illust/details?illust_id=99572709'
+    response = await request.pixiv_proxy(url)
+
+    assert response.status_code == 200
+    assert response.json().get('body')
+
+@pytest.mark.vcr
+async def test_request_like_request(user_with_twitter):
+    post_id = 1544781966993604608
+    like = await request.like_request(post_id, user_with_twitter)
+
+    assert like.status_code == 403
+    assert like.json()['errors'][0]['message'] == 'You have already favorited this status.'
