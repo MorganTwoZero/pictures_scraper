34c5b3a0aeaf3f49fd67c833e802061268c5691d
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-07-26 22:42:08 +0200

    Refactored PostsView using IntersectionAPI

--- frontend/src/views/PostsView.vue
+++ frontend/src/views/PostsView.vue
@@ -2,10 +2,15 @@
   <div v-for="post in posts" :key="post.post_link">
     <PostsComponent :post="post" />
   </div>
+  <div class="loading">
+    <svg viewBox="25 25 50 50">
+      <circle cx="50" cy="50" r="20"></circle>
+    </svg>
+  </div>
 </template>
 
 <script setup>
-import { onBeforeMount, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import router from '@/router'
 
 import PostsComponent from '@/components/PostsComponent.vue'
@@ -15,36 +20,28 @@ import axios from 'axios'
 let posts = ref([]);
 let page = 1;
 
-function debounce(func, wait) {
-    let timeout;
-    return () => {
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-        timeout = setTimeout(func, wait)
-    }
+function setLoadingObserver() {
+
+  const loadingObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) { 
+        getPosts()
+      }
+    })
+  });
+
+  loadingObserver.observe(document.querySelector('.loading'))
 }
 
-function getNextPosts() {
-    if ((window.innerHeight + Math.ceil(window.scrollY)) >= document.body.offsetHeight) { //ceil for mobile
-      axios.get(router.currentRoute.value.fullPath + '?page=' + page).then(response => {
-        posts.value = posts.value.concat(response.data);       
-        page++;
-      });
-    }
-  }
-
-window.onscroll = debounce(getNextPosts, 200);
-
-onBeforeMount(() => {
-  axios.get(router.currentRoute.value.fullPath + '?page=' + page)
-  .then((res) => {
-    posts.value = res.data;
+function getPosts() {
+  axios.get(router.currentRoute.value.fullPath + '?page=' + page).then(response => {
+    posts.value.push(...response.data);
     page++;
-  })
-  .catch((error) => {
-    console.error(error);
   });
+}
+
+onMounted(() => {
+  setLoadingObserver()
 })
 </script>
 
@@ -55,5 +52,20 @@ div {
   border: 1px solid rgb(0, 0, 0);
   width: max-content;
   height: max-content;
+}
+
+.loading {
+  border: 3px solid hsla(185, 100%, 62%, 0.2);
+  border-top-color: #3cefff;
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
