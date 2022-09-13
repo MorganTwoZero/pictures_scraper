24e069ab8422cd660653d4efd98ce9b05c45a7e3
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-09-13 19:14:07 +0200

    Adjusted lofter parser, updated scary authors

--- backend/parsers/lofter.py
+++ backend/parsers/lofter.py
@@ -20,16 +20,16 @@ def parse(db, htmls):
             soup = BeautifulSoup(html, 'html.parser')
 
             for item in soup.find_all(class_='m-mlist'):
-                if item.find(class_='imgc') is not None:
+                if 'data-type' in item.attrs and item.find(class_='imgc'):
                     save_to_db(PostScheme(
-                        author_link=item.find(class_='w-img ptag').a.attrs['href'],
-                        author=item.find(class_='w-img ptag').a.attrs['title'],
-                        author_profile_image=item.find(class_='w-img ptag').img.attrs['src'],
-                        preview_link=item.find(class_='imgc').img.get('src')[:146],
-                        created=datetime.utcfromtimestamp(
-                            int(item.find(class_='isayc').attrs['data-time'][:-3])
-                            ) + timedelta(hours=TIMEZONE),
-                        post_link=item.find(class_='isayc').attrs['href'],
-                        ),
-                    db)
+                    author_link=item.find(class_='w-img ptag').a.attrs['href'],
+                    author=item.find(class_='w-img ptag').a.attrs['title'],
+                    author_profile_image=item.find(class_='w-img ptag').img.attrs['src'],
+                    preview_link=item.find(class_='imgc').img.get('src')[:146],
+                    created=datetime.utcfromtimestamp(
+                        int(item.find(class_='isayc').attrs['data-time'][:-3])
+                        ) + timedelta(hours=TIMEZONE),
+                    post_link=item.find(class_='isayc').attrs['href'],
+                    ),
+                db)
     logger.info('Lofter updated')
