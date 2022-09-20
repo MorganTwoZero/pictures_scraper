6839e2e2663dc1ef382ef0b5b2773d4f047a3b08
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-09-20 07:09:18 +0200

    Simplified deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,6 @@
 ssh root@vpn "cd pictures_scraper && docker compose stop && git pull"
-ssh root@vpn "cd pictures_scraper && docker compose up --build"
+cat .env | ssh root@vpn "cat > pictures_scraper/.env  && cd pictures_scraper && docker compose up --build"
 #clean deploy
 #ssh root@vpn "cd pictures_scraper && docker compose stop && cd .. && rm -rf pictures_scraper && git clone https://github.com/MorganTwoZero/pictures_scraper"
 #ssh root@vpn "mkdir ./pictures_scraper/certs" 
 #ssh root@vpn "cp /etc/letsencrypt/live/pixiv.sbs/fullchain.pem /etc/letsencrypt/live/pixiv.sbs/privkey.pem /root/pictures_scraper/certs -rL"
-#cat .env | ssh root@vpn "cat >> pictures_scraper/.env  && cd pictures_scraper && docker compose up --build"
