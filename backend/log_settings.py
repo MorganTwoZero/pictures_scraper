a4c0c0627416d8b8d9ab53f5aa05b10f11ed8df0
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-12-07 01:49:28 +0200

    refactor: log debugs to file only

--- backend/log_settings.py
+++ backend/log_settings.py
@@ -12,12 +12,14 @@ LOGGING_CONFIG = {
         'stream_handler': {
             'class': 'logging.StreamHandler',
             'formatter': 'default_formatter',
+            'level': 'INFO'
         },
         'logfile': {  # The handler name
             'formatter': 'default_formatter',  # Refer to the formatter defined above
             'class': 'logging.handlers.RotatingFileHandler',  # OUTPUT: Which class to use
             'filename': 'log.txt',  # Param for class above. Defines filename to use, load it from constant
             'backupCount': 2,  # Param for class above. Defines how many log files to keep as it grows
+            'level': 'DEBUG'
         },
     },
     'loggers': {
@@ -31,9 +33,13 @@ LOGGING_CONFIG = {
             'level': 'DEBUG',
             'propagate': True
         },
-        'uvicorn': {
+        'utils': {
             'handlers': ['stream_handler', 'logfile'],
             'level': 'DEBUG',
+            'propagate': True
+        },
+        'uvicorn': {
+            'handlers': ['stream_handler', 'logfile'],
             'propagate': True
         },
     }
