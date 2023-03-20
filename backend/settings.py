d01949d8bfc225852a3d98bcc55dc06cbc092578
root <root@1266511-cq77716.tw1.ru>
2023-03-20 16:41:17 +0300

    updated blacklist

--- backend/settings.py
+++ backend/settings.py
@@ -6,7 +6,7 @@ env_location = Path("../.env").resolve()
 
 class Settings(BaseSettings):
     SCARY_TAGS: list[str] = ['妊娠', '催眠', 'ヒカマニ', 'Hikakin_Mania', '巨大娘', 'COM3D2', 'mikumikudance', 'mmd', 'R18MMD', '崩坏3MMD', 'コイカツ', 'NTR', '動圖', 'R-18G', 'コイカツ!', 'うごイラ', '3D', 'koikatsu!', 'Koikatsu', 'Koikatsu!', '恋活', '3DCG', 'MMD', '王者荣耀', '斗破苍穹', 'koikatsu']
-    SCARY_AUTHORS: list[str]= ['Vulcax', 'hendienuns', '寂静之刃', 'Arise', 'Tomodachi', '梦醒幻实', 'AbilfloridaArts', '四奈乃', '云起', '極彩色', '阡陌菌', '千樺·エイミー・ザル', 'Trishnicajoy', 'PShiro', '黑虎阿符', 'BT1', 'BALD', 'Tay-X', 'Meigane', '过度解毒', '「雪」', '资源出口', '阿曼達·木子', 'MilkMan29', '興趣使然', '肉乎乎の布洛妮娅', '孤人GuRn', ' 哎呀_迷路了_迷路了__']
+    SCARY_AUTHORS: list[str]= ['理香~', '碧梅秋暮君', 'Vulcax', 'hendienuns', '寂静之刃', 'Arise', 'Tomodachi', '梦醒幻实', 'AbilfloridaArts', '四奈乃', '云起', '極彩色', '阡陌菌', '千樺·エイミー・ザル', 'Trishnicajoy', 'PShiro', '黑虎阿符', 'BT1', 'BALD', 'Tay-X', 'Meigane', '过度解毒', '「雪」', '资源出口', '阿曼達·木子', 'MilkMan29', '興趣使然', '肉乎乎の布洛妮娅', '孤人GuRn', ' 哎呀_迷路了_迷路了__']
 
     # Global settings
     TIMEZONE: int = 2
